bc284d929cb1070f68624930ed9ab4f6771b3f52
Edoardo Manieri <edoardomanieri@gmail.com>
2021-02-01 23:18:03 +0000

    remodularize encryption

---
+++ src/passwordmanager/websitePasswords/encryption.py
@@ -0,0 +1,35 @@
+from cryptography.fernet import Fernet
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+def _generate_key(master_password):
+    encoded_master_password = master_password.encode()
+    salt = b"\xb9\x1f|}'S\xa1\x96\xeb\x154\x04\x88\xf3\xdf\x05"
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend())
+    key = base64.urlsafe_b64encode(kdf.derive(encoded_master_password))
+    return key
+
+
+def encrypt(password, master_password):
+    key = _generate_key(master_password)
+    cipher_suite = Fernet(key)
+    cipher_text = cipher_suite.encrypt(password.encode())
+    return cipher_text
+
+
+def decrypt(cipher_text, master_password):
+    key = _generate_key(master_password)
+    cipher_suite = Fernet(key)
+    password_plain = cipher_suite.decrypt(cipher_text).decode()
+    return password_plain
+
+
+
--- src/passwordmanager/websitePasswords/forms.py
+++ src/passwordmanager/websitePasswords/forms.py
@@ -1,12 +1,7 @@
 from django import forms
 from django.forms import fields
-from cryptography.fernet import Fernet
-import base64
-import os
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+from .encryption import encrypt
 from .models import WebsitePassword
 
 class WebsitePasswordForm(forms.ModelForm):
@@ -40,28 +35,8 @@ class WebsitePasswordForm(forms.ModelForm):
         password = self.cleaned_data.get('password')
         if len(password) < 4:
             raise forms.ValidationError("Password must be at least 4 character long")
-        encrypted_password = self._encrypt(password, master_password)
+        encrypted_password = encrypt(password, master_password)
         return encrypted_password
     
 
-    def _encrypt(self, password, master_password):
-        key = self._generate_key(master_password)
-        cipher_suite = Fernet(key)
-        cipher_text = cipher_suite.encrypt(password.encode())
-        # to decode:
-        # key2 = self._generate_key(master_password)
-        # cipher_suite2 = Fernet(key2)
-        # print(cipher_suite2.decrypt(cipher_text).decode())
-        return cipher_text
 
-
-    def _generate_key(self, master_password):
-        encoded_master_password = master_password.encode()
-        salt = b"\xb9\x1f|}'S\xa1\x96\xeb\x154\x04\x88\xf3\xdf\x05"
-        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
-                length=32,
-                salt=salt,
-                iterations=100000,
-                backend=default_backend())
-        key = base64.urlsafe_b64encode(kdf.derive(encoded_master_password))
-        return key
