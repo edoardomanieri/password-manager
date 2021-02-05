c61fd7d87a9e836c3ed2c128a21b48b8ae11d5be
Edoardo Manieri <edoardomanieri@gmail.com>
2021-02-05 14:20:51 +0000

    working token obtaining

--- src/passwordmanager/frontend/src/components/HomePage.js
+++ src/passwordmanager/frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import CreateWebsitePassword from "./CreateWebsitePassword";
+import Login from "./Login";
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -15,6 +16,7 @@ export default class HomePage extends Component {
             <Switch>
                 <Route exact path="/"><p>Home page</p></Route>
                 <Route path='/create' component={CreateWebsitePassword}/>
+                <Route path='/login' component={Login}/>
             </Switch>
         </Router>
         )
--- src/passwordmanager/frontend/urls.py
+++ src/passwordmanager/frontend/urls.py
@@ -3,5 +3,6 @@ from .views import index
 
 urlpatterns = [
     path('', index),
-    path('create/', index)
+    path('create/', index),
+    path('login/', index)
 ]
