55d14d211b54073f5c413e818dfd8ad3e2655db2
Edoardo Manieri <edoardomanieri@gmail.com>
2021-02-07 18:38:03 +0000

    changed image dimension

--- src/passwordmanager/frontend/src/components/HeroBanner.js
+++ src/passwordmanager/frontend/src/components/HeroBanner.js
@@ -19,7 +19,7 @@ function HeroBanner() {
         </div>
 
         <div className="section-image">
-          <img src="../static/images/hero_right.webp" alt="app preview" />
+          <img src="../../static/images/safeapp.jpg" alt="app preview" />
         </div>
       </div>
     </section>
