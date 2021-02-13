8f2652aac36c2f0159578c963c2cb72081824e63
Edoardo Manieri <edoardomanieri@gmail.com>
2021-02-13 16:29:22 +0000

    minor changes

--- src/passwordmanager/frontend/src/components/Login.js
+++ src/passwordmanager/frontend/src/components/Login.js
@@ -146,11 +146,6 @@ export default function Login({ setLogin }) {
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
                 <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
